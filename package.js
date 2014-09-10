64cffaa4a22649f22081cc8a4d47e9b650d118a1
andrej fritz <a.fritz@betzold.de>
2014-09-10 16:24:39 +0200

    updated to meteor 0.9

--- package.js
+++ package.js
@@ -1,5 +1,7 @@
 Package.describe({
-  summary: "Wrapper for node entities"
+  summary: "Wrapper for node entities",
+  version: "0.0.1",
+  git: "http://github.com/andruschka/meteor-entities.git"
 });
     
 Npm.depends({
@@ -7,6 +9,7 @@ Npm.depends({
 });
 
 Package.on_use(function(api) {
+  api.versionsFrom("METEOR@0.9.0");
   api.export("Entities");  
   api.add_files('entities.js', 'server');
 });
